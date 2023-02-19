811505259668b5ef746ac464ca003c66fec71468
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-19 13:59:42 +0530

    Add files via upload

---
+++ BURGERS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y;
+	    cin>>X>>Y;
+	    int M = min(X,Y);
+	    cout<<M<<endl;
+	    T--;
+	}
+	return 0;
+}
---
+++ SQUATS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	int X;
+	while(T>0){
+	    cin>>X;
+	    cout<<X*15<<endl;
+	    T--;
+	}
+	return 0;
+}
