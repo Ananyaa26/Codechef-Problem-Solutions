761a9b24b1bd499de851f0b84d186a7398c5179f
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-20 22:27:57 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/KNGTOR.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int M,N;
+	    cin>>N>>M;
+	    cout<<(N*5+M*7)<<endl;
+	    T--;
+	}
+	return 0;
+}
