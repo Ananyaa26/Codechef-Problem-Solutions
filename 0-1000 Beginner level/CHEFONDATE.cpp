8a735ac944a47008a34583dce2591c81ac1ff3e6
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-21 01:21:54 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/CHEFONDATE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+        int X,Y;
+	    cin>>X>>Y;
+	    if(Y>X){
+	        cout<<"NO"<<endl;
+	    }else{
+	        cout<<"YES"<<endl;
+	    }
+	    T--;
+	}
+	return 0;
+}
