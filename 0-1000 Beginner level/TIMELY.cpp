bad8264c2fd2bb121a67ac5f8cd514e4eb2b6d36
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-20 22:28:29 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/TIMELY.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X;
+	    cin>>X;
+	    if(X>=30){
+	        cout<<"Yes"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	    T--;
+	}
+	return 0;
+}
