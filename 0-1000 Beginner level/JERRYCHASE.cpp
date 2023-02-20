7ab9edadfa4d76a5f23cd81c27fac60abbab0a6a
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-21 01:20:53 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/JERRYCHASE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes hereint T;
+	int T;
+	cin>>T;
+	while(T>0){
+        int X,Y;
+	    cin>>X>>Y;
+	    if(Y>X){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	    T--;
+	}
+	return 0;
+}
