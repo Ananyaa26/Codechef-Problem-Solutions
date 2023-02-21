2328b29369eef49139c77d3540a98cfac40cc8d9
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-21 22:30:11 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/MANAPTS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y;
+	    cin>>X>>Y;
+	    cout<<Y/X<<endl;
+	    T--;
+	}
+	return 0;
+}
