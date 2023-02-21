f43ed2c10dff9e58e33206e9c9dc0a8df8a1301e
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-21 22:28:51 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/WAITTIME.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int K,X;
+	    cin>>K>>X;
+	    cout<<(7*K)-X<<endl;
+	    T--;
+	}
+	return 0;
+}
