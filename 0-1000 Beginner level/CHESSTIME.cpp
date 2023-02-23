34f4c3c3f348edcb47778d532506cbacfa0fd8c9
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-23 19:29:57 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/CHESSTIME.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X;
+	    cin>>X;
+	    cout<<(X*60)/20<<endl;
+	    T--;
+	}
+	return 0;
+}
