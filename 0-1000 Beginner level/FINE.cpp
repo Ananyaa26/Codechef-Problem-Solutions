06a855f3e3da9fabcfb7968d3b4a27e4a6a48a6c
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-23 19:31:36 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/FINE.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	while(N--){
+	    int a;
+	    cin>>a;
+	    if(a<=70)
+	    {
+	        cout<<0<<endl;
+	    }
+	    else if(a>70 && a <=100){
+	        cout<<500<<endl;
+	    }else{
+	        cout<<2000<<endl;
+	    }
+	}
+	
+	return 0;
+	return 0;
+}
