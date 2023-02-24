7450bb1a86eb9f1c1dac6194f2b54372ebb0930b
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-24 16:20:28 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/GDTURN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y;
+	    cin>>X>>Y;
+ 
+	    if(X+Y > 6){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
