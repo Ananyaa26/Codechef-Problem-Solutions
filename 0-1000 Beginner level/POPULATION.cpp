c493ab4960373a96c6a356fec0cae9cf3a097aba
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-26 17:26:31 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/POPULATION.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y,Z;
+	    cin>>X>>Y>>Z;
+        cout<<X-Y+Z<<endl;
+	    T--;
+	}
+	return 0;
+}
