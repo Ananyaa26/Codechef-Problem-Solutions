90eed1b22c04b638f2d32a724a1c110073451216
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-26 17:24:37 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/APPLORNG.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+	    int X,A,B;
+	    cin>>X;
+	    cin>>A>>B;
+ 
+	    if(A+B<=X){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+
+	return 0;
+}
