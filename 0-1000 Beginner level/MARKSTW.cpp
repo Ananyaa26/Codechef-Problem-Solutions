8f4eb21020630d1b68668e0ad58167a4fe155063
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-27 22:34:21 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/MARKSTW.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+	    int X,Y;
+	    cin>>X>>Y;
+ 
+	    if(X>=2*Y){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    
+	
+	return 0;
+}
