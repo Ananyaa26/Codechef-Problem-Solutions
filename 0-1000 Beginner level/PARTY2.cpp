dc460db6ce5f30764c596d5d6bedc6aed49da931
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-02-27 22:33:55 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/PARTY2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int N,X,K;
+	    cin>>N>>X>>K;
+ 
+	    if(N*X<=K){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
