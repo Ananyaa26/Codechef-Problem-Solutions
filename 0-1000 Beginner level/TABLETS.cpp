235916d87a16bb697c72592bc1583b9bb89d323a
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-01 13:06:50 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/TABLETS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y;
+	    cin>>X>>Y;
+ 
+	    if(Y>=3*X){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
