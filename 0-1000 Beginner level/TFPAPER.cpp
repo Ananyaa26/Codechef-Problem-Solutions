cfa7b39e67500de97051feac5d96e2e88b859ca0
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-04 20:00:15 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/TFPAPER.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int K,N;
+	    cin>>N>>K;
+ 
+	    cout<<N-K<<endl;	    
+	    T--;
+	}
+	return 0;
+}
