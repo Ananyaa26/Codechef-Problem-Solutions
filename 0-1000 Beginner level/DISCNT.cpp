4ed45a4119a8eb6fa9964cdc415bf54c483a4348
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-06 23:24:42 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/DISCNT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X;
+	    cin>>X;
+        cout<<100-X<<endl;
+	    	    
+	    T--;
+	}
+	return 0;
+}
