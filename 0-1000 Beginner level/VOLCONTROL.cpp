0c6899408485cdc46080c302c983b706a83f604b
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-07 14:12:39 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/VOLCONTROL.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y;
+	    cin>>X>>Y;
+        cout<<abs(X-Y)<<endl;
+	    T--;
+	}
+	return 0;
+}
