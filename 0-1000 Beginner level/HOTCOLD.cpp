17fec084580216149f2287372816966e9f4c8b48
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-10 01:51:47 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/HOTCOLD.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X;
+	    cin>>X;
+ 
+	    if(X>20){
+	        cout<<"HOT"<<endl;
+	    }else{
+	        cout<<"COLD"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+	return 0;
+}
