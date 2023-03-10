a4250a85e5a02e8f79dc522061211b35038969b8
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-10 13:47:15 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/AGEING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    cout<<X-10<<endl;
+	}
+	return 0;
+}
