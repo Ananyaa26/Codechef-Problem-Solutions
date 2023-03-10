3355b52ab35bffedc5e353fcbf66e1872d0e7736
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-10 13:41:47 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/ONEMORE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X<=24){
+	        cout<<"No"<<endl;
+	    }else{
+	        cout<<"yes"<<endl;
+	    }
+	}
+	return 0;
+}
