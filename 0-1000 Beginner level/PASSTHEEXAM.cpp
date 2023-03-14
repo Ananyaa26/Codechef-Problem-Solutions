d5c81ea68423cd6689de0ec2b29d23ec2dcfb795
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-14 12:23:45 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/PASSTHEEXAM.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+		int T;
+	cin>>T;
+	while(T>0){
+	    int A,B,C;
+	    cin>>A>>B>>C;
+ 
+	    if(A+B+C >= 100 && A>=10 && B>=10 && C>=10){
+	        cout<<"PASS"<<endl;
+	    }else{
+	        cout<<"FAIL"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
