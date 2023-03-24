f869386777b4b96ecfc9b5d2806a691e6709f82b
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-24 19:18:16 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/TVDISC.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+		int T;
+	cin>>T;
+	while(T>0){
+	    int A,B,C,D;
+	    cin>>A>>B>>C>>D;
+        int Tv1 = A-C;
+        int Tv2 = B-D;
+	    if(Tv1 > Tv2){
+	        cout<<"Second"<<endl;
+	    }else if(Tv1 < Tv2){
+	        cout<<"First"<<endl;
+	    }else if(Tv1 == Tv2){
+	        cout<<"Any"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
