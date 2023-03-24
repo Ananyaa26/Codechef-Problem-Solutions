161fe23f7efe95ea2f5e47a70b198aaf8c362671
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-24 19:17:01 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/AIANALYSE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int c;
+	cin>>c;
+	if(c<=1000){
+	    cout<<"yes";
+	}else{
+	    cout<<"no";
+	}
+	return 0;
+}
