403f3bbb2917e6374721ab4ec99a9f34416da2d2
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-25 19:03:02 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/TYRE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y;
+	    cin>>X>>Y;
+        cout<<((X*2)+(Y*4))<<endl;   
+	    T--;
+	}
+	return 0;
+}
