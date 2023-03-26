8b4f8a39fa5a49b12c529539e65f210e0d93bec9
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-26 11:34:51 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/CREDSCORE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X;
+	cin>>X;
+	if(X>=750){
+	    cout<<"Yes"<<endl;
+	}else{
+	    cout<<"No"<<endl;
+	}
+	return 0;
+}
