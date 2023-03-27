3f001f7e8c697f8d8340f53aaa1707bf540e442c
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-28 00:46:04 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/COURSEREG.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int N,M,K;
+	    cin>>N>>M>>K;
+ 
+	    if(M-K>=N){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
