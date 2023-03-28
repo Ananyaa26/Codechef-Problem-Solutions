8891fb7eeb35b82623086dd3ad60a416f3202b52
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-28 16:40:46 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/BATTERYLOW.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X;
+	    cin>>X;
+ 
+	    if(X<=15){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
