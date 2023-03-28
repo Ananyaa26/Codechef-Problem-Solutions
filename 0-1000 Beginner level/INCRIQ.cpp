7f0b2efbc38f96f4be1032b6d1353c0cb8aae2d2
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-28 16:41:11 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/INCRIQ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X;
+	cin>>X;
+	if(X+7>170){
+	    cout<<"Yes";
+	}else{
+	    cout<<"No";
+	}
+	return 0;
+}
