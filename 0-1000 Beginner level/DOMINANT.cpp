886ef993c001c900d308e93364e34b8fdc5f8069
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-29 21:36:53 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/DOMINANT.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y,Z;
+	    cin>>X>>Y>>Z;
+ 
+	    if((X>Y+Z )|| (Y>Z+X)||(Z>X+Y)){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
