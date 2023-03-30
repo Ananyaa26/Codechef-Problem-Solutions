aa6a11e651dd8e8d748748f6018614a0fd750264
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-30 15:37:05 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/COUGAME.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int g,b;
+	    cin>>g>>b;
+	    cout<<b-g<<endl;
+	}
+	return 0;
+}
