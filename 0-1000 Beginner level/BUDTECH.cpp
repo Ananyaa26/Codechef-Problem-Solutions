7e2a603cb583c0e961a8cb0054ae3c5fcd607fbe
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-03-30 15:44:18 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/BUDTECH.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int r;
+	    cin>>r;
+	    int c=r*1000;
+	    int a = c/2;
+	    cout<<a/5<<endl;
+	}
+	return 0;
+}
