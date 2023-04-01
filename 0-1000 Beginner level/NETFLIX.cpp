3e5a218808dad0e2e8664e2ced9b078f997fe66e
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-01 10:17:30 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/NETFLIX.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+		int T;
+	cin>>T;
+	while(T>0){
+	    int X,Y,Z,A;
+	    cin>>X>>Y>>Z>>A;
+ 
+	    if((A<=Y+Z )|| (A<=Z+X)||(A<=X+Y)){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }	    
+	    T--;
+	}
+	return 0;
+}
