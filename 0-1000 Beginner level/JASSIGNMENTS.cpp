068d5b2b8d1be958da0247739e5dd4ea561bd60b
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-02 19:11:58 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/JASSIGNMENTS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(10-X>=3){
+	        cout<<"yES"<<endl;
+	    }else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
