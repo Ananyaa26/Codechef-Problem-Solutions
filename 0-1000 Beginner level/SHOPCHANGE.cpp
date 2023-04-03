51f9c1d530b39ab5a0323f1358def7f6a121bcf6
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-03 11:48:08 +0530

    Add files via upload

---
+++ 0-1000 Beginner level/SHOPCHANGE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    cout<<100-X<<endl;
+	}
+	return 0;
+}
