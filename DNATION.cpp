f0561695015268fe10431e1ac3fc5702a8d6df40
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-07 00:45:06 +0530

    Add files via upload

---
+++ DNATION.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<abs(y-x)<<endl;
+	}
+	return 0;
+}
