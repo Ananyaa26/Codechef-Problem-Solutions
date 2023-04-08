e1df4b7316d2aa3bbebad50c77da09a0cf03bf54
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-08 20:03:44 +0530

    Add files via upload

---
+++ MELTGOLD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int X,Y;
+	    cin>>X>>Y;
+	    cout<<X-Y<<endl;
+	}
+	return 0;
+}
