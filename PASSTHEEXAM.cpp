3989581ebcf5d140b85f9a4359f13e9d34738888
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-20 13:06:15 +0530

    Update and rename 0-1000 Beginner level/PASSTHEEXAM.cpp to PASSTHEEXAM.cpp

--- PASSTHEEXAM.cpp
+++ PASSTHEEXAM.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main() {
 	// your code goes here
-		int T;
+	int T;
 	cin>>T;
 	while(T>0){
 	    int A,B,C;
 	    cin>>A>>B>>C;
- 
 	    if(A+B+C >= 100 && A>=10 && B>=10 && C>=10){
 	        cout<<"PASS"<<endl;
 	    }else{
