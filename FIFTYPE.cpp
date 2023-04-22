aee4f2e946532e714eec6a080c7fc648689e3897
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-22 14:51:19 +0530

    Add files via upload

---
+++ FIFTYPE.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+        int n;
+        cin>>n;
+        int result = 0;
+        while(n!=50){
+             if(n>50){
+                 n-=3;
+                 result++;
+             }
+             else{
+                 n+=2;
+                 result++;
+             }
+         }
+         cout<<result<<endl;
+    }
+    return 0;
+}
