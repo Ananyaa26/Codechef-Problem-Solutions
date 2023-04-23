ac8e2a8a18f74003f339f3cdf3638e70d9415b04
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-24 00:01:05 +0530

    Add files via upload

---
+++ SUGARCANE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int N;
+	    cin>>N;
+	    cout<<50*30*N/100<<endl;
+	}
+	return 0;
+}
