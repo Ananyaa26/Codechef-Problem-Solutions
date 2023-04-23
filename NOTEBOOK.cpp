3e70d50abf5c9005e302307ce8b48274c8d46e4e
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-23 21:12:16 +0530

    Add files via upload

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int N;
+	    cin>>N;
+	    cout<<N*10<<endl;
+	}
+	return 0;
+}
