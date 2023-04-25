c8fca5d6f1e9ae02950d0d444d0d66aa85c62895
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-25 11:14:25 +0530

    Add files via upload

---
+++ THREETOPICS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,x;
+	cin>>a>>b>>c>>x;
+	if(x != a && x!= b && x!=c){
+	    cout<<"No"<<endl;
+	}else{
+	    cout<<"Yes"<<endl;
+	}
+	return 0;
+}
