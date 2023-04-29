a36dd9de90415bb2a605317353f108f7a32a28db
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-04-30 00:53:19 +0530

    Add files via upload

---
+++ CHFRICH.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,x;
+	    cin>>a>>b>>x;
+	    int c=b-a;
+	    cout<<c/x<<endl;
+	}
+	return 0;
+}
