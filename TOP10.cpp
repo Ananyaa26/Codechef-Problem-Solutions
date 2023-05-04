bfc46a2d7f70f9f70510776e5bea5c8aa1103006
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-05-04 17:16:11 +0530

    Add files via upload

---
+++ TOP10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X>10){
+	        cout<<"NO"<<endl;
+	    }else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
