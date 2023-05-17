a44b0abf263d5688c6f38e560701a11b12fa2eee
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-05-17 22:28:47 +0530

    Add files via upload

---
+++ THREEAPFREE.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int num[n];
+	    for(int i=0;i<n;i++){
+	        cin>>num[i];
+	    }
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if ((num[j] - num[i]) == (num[k] - num[j])) {
+                        cout<<"No";
+	                
+	                }
+	            }
+	        }
+	    }
+	return 0;
+}
