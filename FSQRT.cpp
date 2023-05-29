e01802b1ab91dd4c449595e64c996e08140ad75b
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-05-29 19:51:01 +0530

    Add files via upload

---
+++ FSQRT.cpp
@@ -0,0 +1,26 @@
+//  We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin >> t;
+//   if (t >= 1 && t <= 20)
+//   {
+      while (t--)
+      {
+         int n;
+         cin >> n;
+        //  if(n>=1 && n<=10000)
+        //  {
+            cout << int(sqrt(n)) << endl;
+        //  }
+         
+      }
+//   }
+
+   return 0;
+}
