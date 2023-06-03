407453a0bf7c3fb5aaf786e6afc7382326e308b8
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-06-03 11:42:11 +0530

    Add files via upload

---
+++ VALENTINE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<x/y<<endl;
+	}
+	return 0;
+}
