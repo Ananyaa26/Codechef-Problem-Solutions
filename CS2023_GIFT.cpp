8c3a81085b537c3d5bac019e2be5691966294767
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-06-17 20:30:10 +0530

    Add files via upload

---
+++ CS2023_GIFT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,m,n;
+	cin>>x>>n>>m;
+	if(x+m>=n){
+	    cout<<"YES"<<endl;
+	}else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
