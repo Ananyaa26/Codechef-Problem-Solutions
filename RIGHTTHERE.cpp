3f0bf538e00c74af724be7c63a0befb0728d2d70
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-06-18 14:13:20 +0530

    Add files via upload

---
+++ RIGHTTHERE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    if(n<=x){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
