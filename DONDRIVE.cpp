9d01de162a91d661eb89669ba9c14d27927e1683
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-06-21 22:11:27 +0530

    Add files via upload

---
+++ DONDRIVE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    cout<<n-x<<endl;
+	}
+	return 0;
+}
