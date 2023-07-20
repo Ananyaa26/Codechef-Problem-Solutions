52af95403d170737d8a7b785d00a2202c66cc151
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-07-20 18:15:48 +0530

    Add files via upload

---
+++ PUZHUNT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>=6 && n<=8){
+	    cout<<"YES"<<endl;
+	}else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
