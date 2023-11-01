41be6718ae7ece0728ccc842c61a56a287c64e5b
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-01 22:12:14 +0530

    Add files via upload

---
+++ FIZZBUZZ2301.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B,C;
+    cin>>A>>B>>C;
+    if(A>B && A>C){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
