b3fd8b27d4e92bb23721d9e67e6f9f3e2b6a020d
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-01 22:13:00 +0530

    Add files via upload

---
+++ FIZZBUZZ2303.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int val = n*(n-1);
+        cout<<val<<endl;
+    }
+}
