8a5cd57a2f4a176404fd2dcfd14f878447e8f925
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-08 22:03:15 +0530

    Add files via upload

---
+++ PERMOR.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=n;i>0;i--){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
