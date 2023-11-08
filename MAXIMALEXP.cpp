045f4b80bc0fa5f34fbf40a3bd96f9831b120319
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-08 22:03:32 +0530

    Add files via upload

---
+++ MAXIMALEXP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        
+    }
+}
