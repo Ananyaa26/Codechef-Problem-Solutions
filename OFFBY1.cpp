23d7719e2857c5f73db29bcdededb0472f32066f
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-10 15:02:26 +0530

    Add files via upload

---
+++ OFFBY1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    cin>>a>>b;
+    int val = (a+b)*10+1;
+    cout<<val;
+    
+}
