0e2df52fb1c7c63a543205395380931d0d80b025
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-10 15:03:30 +0530

    Add files via upload

---
+++ NEWSPAPER.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    
+    int x;
+    cin>>x;
+    if(x==8 || x==9 || x==10){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+}
