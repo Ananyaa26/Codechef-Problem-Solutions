c3bbb3c0ad132ee0922fd9d67ce10769c58a83be
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-10 15:07:53 +0530

    Add files via upload

---
+++ AIRINDEX.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    
+    int x;
+    cin>>x;
+    if(x<100){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+}
