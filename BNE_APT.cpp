e417f01b84678039d68bf76f4ff80ca238eef549
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-10 15:02:51 +0530

    Add files via upload

---
+++ BNE_APT.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int x,y;
+    cin>>x>>y;
+    cout<<(x*n)+(y*m);
+}
