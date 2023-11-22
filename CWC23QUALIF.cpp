0edfed95ede2c0c87858650cf39e5d9f14ade9f9
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-22 21:45:07 +0530

    Add files via upload

---
+++ CWC23QUALIF.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    
+    int t;
+    cin>>t;
+    if(t>=12){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
