f89c4d062038776969ee5fc193d6c2ac9e97996b
Ananyaa Bansal <89255668+Ananyaa26@users.noreply.github.com>
2023-11-22 21:44:25 +0530

    Add files via upload

---
+++ VIDEOWORTH.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int s;
+        cin>>s;
+        cout<<24*1000*s<<endl;
+    }
+}
